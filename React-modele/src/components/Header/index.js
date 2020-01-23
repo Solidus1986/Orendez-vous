595968eea13f96efd16c72d8069ba8545033633b
Solidus1986 <jonathan.bibinet@gmail.com>
2020-01-23 20:28:35 +0100

    for header, i add route for home with link and logo

--- React-modele/src/components/Header/index.js
+++ React-modele/src/components/Header/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Image } from 'semantic-ui-react';
 
 import './header.scss';
+import logo from 'src/Images/logo.png';
 
 
 const Header = () => (
   <div className="header">
-    <Link to="/" className="logo">logo du site</Link>
+    <Link to="/" className="logo"><Image src={logo} size="big" circular /></Link>
     <Link to="/profil" className="profil">compte utilisateur</Link>
     <Link to="/connexion" className="profil">Connexion</Link>
   </div>
