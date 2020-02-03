e389c91eb79e6097c9a0345708e8331ce655e51a
gauthier-dallest <gauthierdu34@laposte.net>
2020-02-03 17:04:38 +0100

    fix wordpress home

--- WP/README.md
+++ WP/README.md
@@ -6,10 +6,11 @@
 5. Compléter dans le fichier `wp-config.php` (copie de `wp-config-sample.php`):
    1. Les informations de connexion à la base de données
    2. Les clés de salage
-   3. L'URL de la page d'accueil
-   4. Passer la constante `WP_DEBUG` à `true`
-   5. Sélectionner l'environnement souhaité (`production`, `development` ou `staging`)
-   6. La clé de salage pour JWT (ligne 170)
+   3. L'URL de la partie front du site
+   4. L'URL de la page d'accueil
+   5. Passer la constante `WP_DEBUG` à `true`
+   6. Sélectionner l'environnement souhaité (`production`, `development` ou `staging`)
+   7. La clé de salage pour JWT (ligne 172)
 6. Modifier les droits des dossiers et fichiers avec les commandes
     ```
     sudo chown -R <user>:www-data .
--- WP/content/themes/orendezvous/index.php
+++ WP/content/themes/orendezvous/index.php
@@ -1,1 +1,23 @@
-Je suis sur index.php
+<!DOCTYPE html>
+<html lang="<?php bloginfo('language'); ?>">
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin Wordpress</title>
+    <?php wp_head(); ?>
+</head>
+<body>
+    <h1>Bienvenue sur la partie Wordpress de o'Rendez-vous</h1>
+
+    <a href="<?= FRONT_URL ?>">Retour vers la partie client</a>
+    <?php if(is_user_logged_in()) :?>
+    <a href="admin">Accès à l'administration</a>
+    <a href="wp/wp-login.php?action=logout">Déconnexion</a>
+    <?php else : ?>
+    <a href="admin">Connexion</a>
+    <?php endif; ?>
+    <?php wp_footer(); ?>
+</body>
+</html>
+
