b88c3b50889d67e8a511f577be2b5e6673bd0be5
Simon Rulquin <simon.rulquin@gmail.com>
2020-02-05 12:24:37 +0100

    Reservation axios done

--- React-modele/src/components/Reservation/index.js
+++ React-modele/src/components/Reservation/index.js
@@ -118,17 +118,37 @@ useEffect(() => {
       .catch(e => console.log(e));
   },[selectPractitioner])
 
-  useEffect(()=>{
+  // useEffect(()=>{
     
-    axios.post(`${WP_URL}${HORAIRES_URL}id=${selectDate}`, {}, {
-      headers: { Authorization: `Bearer ${localStorage.getItem('token')}`}
-  })
-      .then(res => {
-        console.log('dates', res);
-        selectDate(res.data)
-      })
-      .catch(e => console.log(e));
-  },[selectDate])
+  //   axios.post(`${WP_URL}${HORAIRES_URL}id=${selectDate}`, {}, {
+  //     headers: { Authorization: `Bearer ${localStorage.getItem('token')}`}
+  // })
+  //     .then(res => {
+  //       console.log('dates', res);
+  //       selectDate(res.data)
+  //     })
+  //     .catch(e => console.log(e));
+  // },[selectDate])
+
+
+  //onSubmit = e => {
+  //  e.preventDefault();
+  //  // PROCESS FORM //
+  //  // const { values } = this.props;
+  //  const values = dates[date][d].id 
+  //  
+//
+  //  axios.post('http://ec2-54-243-1-38.compute-1.amazonaws.com/wordpress/wp-json/wp/v2/users/appointments',values, {
+  //    headers: { 'Authorization': 'Bearer ' + app.getToken() }
+  //  })
+  //    .then((result) => {
+  //      console.log(result)
+//
+  //    })
+  //    .catch((error) => {
+  //      console.error('c\'est une erreur', error.response);
+  //    });
+  //};
 
 // ------------------> HANDLE <-----------------------
 
@@ -151,12 +171,21 @@ useEffect(() => {
 
   const handleSubmit = event => {
     event.preventDefault();
-    { logged ? (
-      onSubmit(selectDate)
-    ) : (
-     alert(<Link to="/connexion" className="profil">connexion</Link>) 
-    )}
-    
+    const bookingForm = event.target;
+    const bookingFormData = new FormData(bookingForm);
+    const data_appointment = {};
+    data_appointment.appointment_id = bookingFormData.get('position');
+      axios.post(`${WP_URL}${HORAIRES_URL}`, data_appointment ,{
+        headers: { Authorization: `Bearer ${localStorage.getItem('token')}`}
+    })
+        .then(res => {
+          console.log('dates', res);
+          alert('Votre réservation est validé')
+          res.redirect('/profil')
+        })
+        .catch(e => {
+          console.log('c\'est une erreur', e.response)
+        });
   }
 
   const cleanDate = (date) => {
@@ -219,10 +248,12 @@ useEffect(() => {
         <Grid item xs={12}>
           <h2 style={{margin:'1rem'}} className="datum">Choisir votre date de rendez-vous</h2>
         </Grid>
-        
+
         <Grid item xs={6}>
+        <form onSubmit={handleSubmit}>
         {Object.keys(dates).map((date,index) => (
           <ExpansionPanel
+            key={index}
             className={classes.panel}
           >
             <ExpansionPanelSummary
@@ -237,8 +268,8 @@ useEffect(() => {
             </ExpansionPanelSummary>
               <ExpansionPanelDetails>
               {Object.keys(dates[date]).map((d, index) => (
-                <RadioGroup aria-label="position" name="position" value={selectDate} onChange={handleChangeDate} row>
-                {console.log('quelle heure est t-il?', cleanDate(dates[date][d].start_date))}
+                <RadioGroup key={index} aria-label="position" name="position" value={selectDate} onChange={handleChangeDate} row>
+                {/* {console.log('quelle heure est t-il?', cleanDate(dates[date][d].start_date))} */}
                     <FormControlLabel
                       key={index}
                       value={dates[date][d].id}
@@ -254,13 +285,15 @@ useEffect(() => {
           </ExpansionPanel>
         ))} 
           <Button
-            type='button'
-            variant='outline'
+            type='submit'
+            // variant='outline'
             className={classes.button}
-            onSubmit={handleSubmit}
+            
           >VALIDATION
-          </Button>
+          </Button> 
+          </form>
         </Grid>
+       
     </Grid>
   </Grid>
 </>
